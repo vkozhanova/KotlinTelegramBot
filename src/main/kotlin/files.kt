b4f28c615a96ffed588cee481c93aeef8d3c9355
vkozhanova <kozhanova_vera@mail.ru>
2025-01-17 16:28:25 +0500

    KTB-02

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -5,6 +5,5 @@ import java.io.File
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.readLines().forEach { println(it) }
+    wordsFile.forEachLine { println(it) }
 }
